b07e0e5f599ce4809773cc0b6c3897ca5e7ac21b
Antoine Salesse <antoine_salesse@ext.connect-tech.sncf>
2023-01-08 17:37:16 +0100

    feat: better perf for ex8 (more read-IOs and less write-IOs) by using matrix pointers instead of copying values

--- app/src/main/kotlin/algo/exemple8_matrice_liée.kt
+++ app/src/main/kotlin/algo/exemple8_matrice_liée.kt
@@ -16,29 +16,30 @@ fun main() {
     printMatrixElementsInOrder(matrix)
 }
 
-private data class MutableInt(var value: Int = 0) {
+private data class MutableInt(var value: Int = 0) : Comparable<MutableInt> {
     fun incrementAndGet() = ++value
+    fun get() = value
+    override fun compareTo(other: MutableInt): Int = get().compareTo(other.get())
 }
 
-// Maintient un index trié du premier élément non affiché de chaque ligne de la matrice.
-// Affiche le premier élément de l'index (le plus petit), puis le supprime de l'index.
-// Ensuite, on ajoute dans l'index l'élément suivant de la même ligne de la matrice que l'élément tout juste affiché.
-// Le nouvel élément est automatiquement et efficacement placé au bon endroit dans l'index
-// On recommence jusqu'à ce que l'index soit vide
+// Maintient une PriorityQueue (équivalent d'une TreeList, soit une liste ordonnée suivant un Comparator) de l'index du premier élément non affiché de chaque ligne de la matrice.
+// Affiche le premier élément de la queue (le plus petit), puis l'en supprime.
+// Ensuite, on ajoute dans la queue l'index de l'élément suivant de la même ligne de la matrice que l'élément tout juste affiché.
+// Le nouvel index d'élément est automatiquement et efficacement placé au bon endroit dans la queue
+// On recommence jusqu'à ce que la queue soit vide
 fun printMatrixElementsInOrder(matrix: Array<IntArray>) {
-    val lineIndexToCursor: MutableMap<Int, MutableInt> = mutableMapOf()
-    val lineIndexToValueAtItsCurrentCursor: PriorityQueue<Pair<Int, Int>> = matrix
+    val lineIndexToCursor: PriorityQueue<Pair<Int, MutableInt>> = matrix
+        .indices
         .asSequence()
-        .mapIndexed { index, intArray -> index to intArray[0] }
-        .toCollection(PriorityQueue(matrix.size, comparing { it.second }))
-    while (lineIndexToValueAtItsCurrentCursor.isNotEmpty()) {
-        val minValueByItsLineIndex: Pair<Int, Int>? = lineIndexToValueAtItsCurrentCursor.poll()
+        .map { lineIndex -> lineIndex to MutableInt() }
+        .toCollection(PriorityQueue(matrix.size, comparing { matrix[it.first][it.second.value] }))
+    while (lineIndexToCursor.isNotEmpty()) {
+        val minValueByItsLineIndex: Pair<Int, MutableInt>? = lineIndexToCursor.poll()
         if (minValueByItsLineIndex != null) {
-            print("${minValueByItsLineIndex.second} ")
-            val cursorForLineIndex: MutableInt = lineIndexToCursor.getOrPut(minValueByItsLineIndex.first) { MutableInt() }
-            val replacement: Int? = matrix[minValueByItsLineIndex.first].getOrNull(cursorForLineIndex.incrementAndGet())
-            if (replacement != null) {
-                lineIndexToValueAtItsCurrentCursor.offer(minValueByItsLineIndex.first to replacement)
+            print("${matrix[minValueByItsLineIndex.first][minValueByItsLineIndex.second.get()]} ")
+            val replacementIndex: Int = minValueByItsLineIndex.second.incrementAndGet()
+            if (replacementIndex < matrix[minValueByItsLineIndex.first].size) {
+                lineIndexToCursor.offer(minValueByItsLineIndex.first to minValueByItsLineIndex.second)
             }
         }
     }
